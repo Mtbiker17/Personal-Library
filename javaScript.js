460b780b3ee9d27dbc4f727493719bb0ff2508f7
Devin <ospreywebservices@gmail.com>
2021-02-11 03:23:49 -0500

    started function to add book object to empty array, added modal functions

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,8 @@
+let addBookButton = document.getElementById('addBookButton');
+let bookTitle = document.getElementById("bookTitle");
+let bookAuthor = document.getElementById("bookAuthor");
+let modal = document.getElementById("bookModal");
+var span = document.getElementsByClassName("close")[0];
 let myLibrary = [];
 
 function Book(){
@@ -6,6 +11,37 @@ function Book(){
 }
 
 function addBookToLibrary(){
-    
+    let title = document.getElementById("bookTitle").value;
+    let author = document.getElementById("bookAuthor").value;
+    let read = document.getElementById("read").value;
+    let pages = document.getElementById("pages").value;
+    let recommend = document.getElementById("recommend").value;
 
+    if(title === "" || author === ""){
+        alert("Book Title and Author Required");
+        return;
+    }
+    modal.style.display = "block";
+        let newBook = {
+        title: title,
+        author: author,
+        read: read,
+        pages: pages,
+        recommend: recommend
+    }
+    myLibrary.push(newBook);
+    console.log(newBook);
+    console.log(myLibrary);
 }
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+addBookButton.addEventListener('click', addBookToLibrary);
