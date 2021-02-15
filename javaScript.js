e5709f53c5c92648d88aa18945c0caf919fd7ab2
Devin <ospreywebservices@gmail.com>
2021-02-14 19:52:42 -0500

    added modal save book input, restructred book info object storage

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,8 @@
 let addBookButton = document.getElementById('addBookButton');
-let bookTitle = document.getElementById("bookTitle");
-let bookAuthor = document.getElementById("bookAuthor");
+
 let modal = document.getElementById("bookModal");
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
+let saveBook = document.getElementById('saveBookInfo');
 let myLibrary = [];
 
 function Book(){
@@ -11,18 +11,22 @@ function Book(){
 }
 
 function addBookToLibrary(){
-    let title = document.getElementById("bookTitle").value;
-    let author = document.getElementById("bookAuthor").value;
-    let read = document.getElementById("read").value;
-    let pages = document.getElementById("pages").value;
-    let recommend = document.getElementById("recommend").value;
-
-    if(title === "" || author === ""){
+let bookTitle = document.getElementById("bookTitle").value;
+let bookAuthor = document.getElementById("bookAuthor").value;
+if(bookTitle === "" || bookAuthor === ""){
         alert("Book Title and Author Required");
         return;
     }
     modal.style.display = "block";
-        let newBook = {
+}
+
+function saveBookInfo(){
+  let title = document.getElementById("bookTitle").value;
+  let author = document.getElementById("bookAuthor").value;
+  let read = document.getElementById("read").value;
+  let pages = document.getElementById("pages").value;
+  let recommend = document.getElementById("recommend").value;
+  let newBook = {
         title: title,
         author: author,
         read: read,
@@ -34,14 +38,11 @@ function addBookToLibrary(){
     console.log(myLibrary);
 }
 
+
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
 
+saveBook.addEventListener('click', saveBookInfo);
 addBookButton.addEventListener('click', addBookToLibrary);
