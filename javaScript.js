b9ba216ce6e5b0485f71ba86d939da290141138e
Devin <ospreywebservices@gmail.com>
2021-02-26 20:35:11 -0500

    building remove functionality, work in progress

--- javaScript.js
+++ javaScript.js
@@ -15,27 +15,27 @@ function bookInfoCheck() {
   modal.style.display = "block";
 }
 
-function Book(title, author, pages, read, recommend, bookNumber) {
-    this.title = title,
-    this.author = author,
-    this.read = read,
-    this.pages = pages,
-    this.recommend = recommend
-    this.bookNumber = bookNumber;
-    this.displayInfo();
-}
-
 function addBookToLibrary() {
   let title = document.getElementById("bookTitle").value;
   let author = document.getElementById("bookAuthor").value;
   let read = document.getElementById("read").value;
   let pages = document.getElementById("pages").value;
   let recommend = document.getElementById("recommend").value;
-  let bookNumber = myLibrary.length + 1;
+  let bookNumber = myLibrary.length;
   const addBook = new Book(title, author, pages, read, recommend, bookNumber);
   myLibrary.push(addBook);
   console.log(myLibrary);
   modal.style.display = "none";
+}
+
+function Book(title, author, pages, read, recommend, bookNumber) {
+  this.title = title,
+  this.author = author,
+  this.read = read,
+  this.pages = pages,
+  this.recommend = recommend
+  this.bookNumber = bookNumber;
+  this.displayInfo();
 }
 
 Book.prototype.displayInfo = function () {
@@ -45,28 +45,28 @@ Book.prototype.displayInfo = function () {
   let pagesInfo = document.createElement("span");
   let recommendInfo = document.createElement("span");
   let removeBook = document.createElement("button");
-  removeBook.setAttribute("class", 'removeButton');
-  removeBook.setAttribute("id", `${this.bookNumber}`);
-  newDiv.setAttribute("id", `${this.bookNumber}`);
-
-  console.log(removeBook.id);
-  
   bookContainer.append(newDiv);
   newDiv.classList.add('book');
+  newDiv.setAttribute("id", `${this.bookNumber}`);
+  
   newDiv.appendChild(titleInfo).classList.add('newBook');
   newDiv.appendChild(authorInfo).classList.add('newBook');
   newDiv.appendChild(pagesInfo).classList.add('newBook');
   newDiv.appendChild(recommendInfo).classList.add('newBook');
   newDiv.appendChild(removeBook);
+  removeBook.setAttribute("class", 'removeButton');
+  removeBook.setAttribute("id", `${this.bookNumber}`);
   titleInfo.textContent = `Title: ${this.title}`;
   authorInfo.textContent = `Author: ${this.author}`;
   pagesInfo.textContent = `# of Pages: ${this.pages}`;
   recommendInfo.textContent = `Recommend This Book?: ${this.recommend}`;
   removeBook.textContent = "Remove"
-
+  
   removeBook.addEventListener('click', () => {
-    myLibrary.splice(0, removeBook.id);
-    console.log(removeBook.id);
+    let removeId = removeBook.id;
+    let removeDiv = Book.bookNumber;
+    myLibrary = myLibrary.filter(library => removeId !== removeDiv);
+    console.log(myLibrary, removeId, removeDiv);
   })
 }
 
